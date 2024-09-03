63901fdf728bd85874798ce4bae672b7dab73f12
Ducky <dreadducky@protonmail.com>
2024-09-03 01:44:23 -0400

    Error build state with C++

--- leetcode/top150/88-merge-sorted-arrays/c++/merge_arrays.cpp
+++ leetcode/top150/88-merge-sorted-arrays/c++/merge_arrays.cpp
@@ -1,4 +1,4 @@
-#include "templates/include/solution.hpp"
+#include "templates/c++/include/solution.hpp"
 
 using namespace solution;
 
@@ -8,48 +8,48 @@ Solution::Solution() {};
 
 void Solution::solution(input<vector<int>>* inputs) {
         // Initialize empty vector
-        vector<int> merged_nums (0, m+n);
+        vector<int> merged_nums (0, inputs->num_inputs+(inputs + 1)->num_inputs);
 
         // Max potential size of array
-        int max_size = nums1.num_inputs + nums2.num_inputs;
+        int max_size = inputs->num_inputs + (inputs + 1)->num_inputs;
 
         // Iterators
         int i = 0; 
         int j = 0;
 
         // Loop through both arrays based on order
-        while (i < nums1.num_inputs || j < nums2.num_inputs) {
+        while (i < inputs->num_inputs || j < (inputs + 1)->num_inputs) {
             // Break conditions
-            if (i == nums1.num_inputs) {
-                for (int k = j; k < nums2.num_inputs; ++k) {
-                    merged_nums.push_back(nums2.inputs[k]);
+            if (i == inputs->num_inputs) {
+                for (int k = j; k < (inputs + 1)->num_inputs; ++k) {
+                    merged_nums.push_back((inputs + 1)->inputs[k]);
                 }
                 break;
             }
-            else if (j == num2s.num_inputs) {
-                for (int k = i; k < nums1.nums_inputs; ++k) {
-                    merged_nums.push_back(nums1.inputs[k]);
+            else if (j == (inputs + 1)->num_inputs) {
+                for (int k = i; k < inputs->num_inputs; ++k) {
+                    merged_nums.push_back(inputs->inputs[k]);
                 }
                 break;
             }
             // Ordering
-            if (nums1.inputs[i] < nums2.inputs[j]) {
-                merged_nums.push_back(nums1.inputs[i]);
+            if (inputs->inputs[i] < (inputs + 1)->inputs[j]) {
+                merged_nums.push_back(inputs->inputs[i]);
                 ++i;
             } 
-            else if (nums1[i] > nums2[j]) {
-                merged_nums.push_back(nums2.inputs[j]);
+            else if (inputs->inputs[i] > (inputs + 1)->inputs[j]) {
+                merged_nums.push_back((inputs + 1)->inputs[j]);
                 ++j;
             } 
             else {
-                merged_nums.push_back(nums1.inputs[i]);
-                merged_nums.push_back(nums2.inputs[j]);
+                merged_nums.push_back(inputs->inputs[i]);
+                merged_nums.push_back((inputs + 1)->inputs[j]);
                 ++i;
                 ++j;
             }
         }
         // Return results in first array
-        nums1.inputs.swap(merged_nums);
+        inputs->inputs.swap(merged_nums);
 }
 
 output<vector<int>> Solution::solution(inputs<vector<int>>* inputs) {
