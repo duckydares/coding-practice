6290d483fd19583b8e373a8a85030b914897e521
Ducky <dreadducky@protonmail.com>
2024-09-10 00:46:15 -0400

    Passed unit tests
    Updated scripts

--- leetcode/top150/88-merge-sorted-arrays/python/solution.py
+++ leetcode/top150/88-merge-sorted-arrays/python/solution.py
@@ -8,35 +8,37 @@ class Solution(SolutionBase):
 
     def solution(self):
         """Expected input order: m, n, nums1, nums2"""
-        solution = []
+        output = []
         # Max potential size of array
         max_size = self.inputs[0] + self.inputs[1]
-        i, j = 0
+        i = 0
+        j = 0
         
         while (i < self.inputs[0] or j < self.inputs[1]):
             # Break conditions
             if (i == self.inputs[0]):
                 for k in range(j, self.inputs[1]):
-                    solution.append(self.inputs[3][k])
+                    output.append(self.inputs[3][k])
                 break
-            elif (j == self.inputs[1][1]):
+            elif (j == self.inputs[1]):
                 for k in range(i, self.inputs[0]):
-                    solution.append(self.inputs[2][k])
+                    output.append(self.inputs[2][k])
                 break
             # Ordering
-            if (self.inputs[0][i] < self.inputs[1][j]):
-                solution.append(self.inputs[0][i])
+            if (self.inputs[2][i] < self.inputs[3][j]):
+                output.append(self.inputs[2][i])
                 i = i + 1
-            elif (self.inputs[0][i] > self.inputs[1][j]):
-                solution.append(self.inputs[1][j])
+            elif (self.inputs[2][i] > self.inputs[3][j]):
+                output.append(self.inputs[3][j])
                 j = j + 1
             else:
-                solution.append(self.inputs[0][i])
-                solution.append(self.inputs[1][j])
+                output.append(self.inputs[2][i])
+                output.append(self.inputs[3][j])
                 i = i + 1
                 j = j + 1
 
-        return solution
+        self.logger.info(f'Output is {output}')
+        return output
 
 
 
--- scripts/pytest_all.sh
+++ scripts/pytest_all.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 # This script runs pytest on all problems solved
+LOG=false
 TEST_FILES=$(find -type f -name test.py)
 
+if [ $# -eq 1 ]; then
+    if [ $1 == '-s' ]; then
+        LOG=true
+    elif [ $1 == '--help' ]; then
+        echo "Usage:"
+        echo "scripts/pytest_all.sh -s"
+        echo "-s: disables output capturing for logging"
+        exit 0
+    fi
+fi
 for TEST in ${TEST_FILES}; do
-    pytest -v $TEST
+    if $LOG; then
+        pytest -v --log-cli-level=DEBUG --log-cli-format="%(asctime)s - %(name)s - %(levelname)s - %(message)s" $TEST
+    else
+        pytest -v $TEST
+    fi
 done
