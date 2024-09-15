cdc55e4d3c10ba9169b0d8b89b77632342036361
Ducky <dreadducky@protonmail.com>
2024-09-15 18:57:20 -0400

    Passed solution that is inplace with constant auxillary memory complexity

--- leetcode/top150/88-merge-sorted-arrays/c++/solution.cpp
+++ leetcode/top150/88-merge-sorted-arrays/c++/solution.cpp
@@ -58,37 +58,37 @@ void Solution::solution(std::vector<std::shared_ptr<output<std::vector<int>>>>& 
         outputs[0]->outputs.swap(merged_nums);
 }
 
-
-// template<>
-// std::vector<std::shared_ptr<output<std::vector<int>>>> Solution::solution(int n_outputs, )
 // Fill in this solution as well if an inplace solution is required.
 /*
 This solution returns the solution into the referenced input variable reducing the auxillary memory to O(1)
+and still maintains a runtime complexity of O(m + n)
 */
 template <>
 void Solution::inplace_solution(std::vector<std::shared_ptr<input<int, std::vector<int>>>>& inputs) {
     // Iterator
-    int i = 0;
+    int j = 0;
     
     // Loop through nums1 array since nums1.size() >= nums2.size() always
-    for (auto it = std::get<1>(inputs[0]->inputs).begin(); 
-         it != std::get<1>(inputs[0]->inputs).end(); ++it) {
-             // Break condition
-             if (i == std::get<0>(inputs[1]->inputs)) {
-                 return;
-             }
-             // Insert nums2[j] if <= to current iterator
-             if (std::get<1>(inputs[1]->inputs)[i] <= *it) {
-                 std::get<1>(inputs[0]->inputs).insert(it, std::get<1>(inputs[1]->inputs)[i]);
-                 ++i;
-             }
-         }
-    // Check that i != 0 otherwise push back all elements of nums2 into nums1 since nums2[i] > nums1[j] for all 0 < i < n, 0 < j < m
-    if (i == 0) {
-        auto it = std::get<1>(inputs[0]->inputs).begin();
-        it = it + std::get<0>(inputs[0]->inputs);
-        for (; i < std::get<0>(inputs[1]->inputs); ++i) {
-            std::get<1>(inputs[0]->inputs).push_back(std::get<1>(inputs[1]->inputs)[i]);
+    for (int i = 0; i < std::get<0>(inputs[0]->inputs) + std::get<0>(inputs[1]->inputs); ++i) {
+        // Break condition
+        if (j == std::get<0>(inputs[1]->inputs)) {
+            return;
+        }
+        // Insert nums2[j] if <= nums1[i]
+        if (std::get<1>(inputs[1]->inputs)[j] <= std::get<1>(inputs[0]->inputs)[i]) {
+            auto it = std::get<1>(inputs[0]->inputs).begin() + i;
+            std::get<1>(inputs[0]->inputs).insert(it, std::get<1>(inputs[1]->inputs)[j]);
+            std::get<1>(inputs[0]->inputs).pop_back();
+            ++j;
+        }
+    }
+    // Check that j != n otherwise push back all elements of nums2 into nums1 since nums2[i] > nums1[j] for all 0 < i < n, 0 < j < m
+    if (j != std::get<0>(inputs[1]->inputs)) {
+        for (; j < std::get<0>(inputs[1]->inputs); ++j) {
+            auto it = std::get<1>(inputs[0]->inputs).begin();
+            it = it + std::get<0>(inputs[0]->inputs) + j;
+            std::get<1>(inputs[0]->inputs).insert(it, std::get<1>(inputs[1]->inputs)[j]);
+            std::get<1>(inputs[0]->inputs).pop_back();
         }
     }
     return;
