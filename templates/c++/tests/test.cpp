803e6f9e37d34cf1a6d6efaea90bcf510a4caae3
Ducky <dreadducky@protonmail.com>
2024-09-20 16:37:14 -0400

    Updated templates

--- templates/c++/tests/test.cpp
+++ templates/c++/tests/test.cpp
@@ -4,14 +4,20 @@
 TEST(template_name, test_case_1) {
     // Declare variables
 
+    std::shared_ptr<solution::input<>> input_ptr = 
+        std::make_shared<solution::input<>>();
+
+    std::vector<std::shared_ptr<solution::input<>>> inputs{input_ptr};
+    
     // Expected output
+    solution::output<> expected();
 
     // Instantiate solver
     solution::Solution solver;
 
     // Solve problem
-    auto result = solver.solution(); // Use either or both and compare / Add in variables
-    solver.inplace_solution(); // Add variables
+    auto result = solver.solution<>(1, inputs); // Use either or both and compare / Add in variables
+    solver.inplace_solution(inputs); // Add variables
 
     // Compare results against expected
     EXPECT_EQ(); // Add in variables for comparison
